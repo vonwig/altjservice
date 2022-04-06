fe50a2b6278b49893e5b0a9488dcfdfb59674ead
Jim Clark <jim@atomist.com>
2022-04-06 12:17:05 -0700

    next version -> 198

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :description "docker test"
   :url "https://github.com/atomisthq/bot-service"
 
-  :dependencies [[ring/ring-core "1.8.2"]]
+  :dependencies [[ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]]
 
   :exclusions [commons-logging log4j org.slf4j/slf4j-log4j12]
 
--- src/atomist/main.clj
+++ src/atomist/main.clj
@@ -5,7 +5,7 @@
 (defn handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (-> {:version 197}
+   :body (-> {:version 198}
              (str))})
 
 (defn -main [& args]
